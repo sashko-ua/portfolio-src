b6bdf2995d8a5f092bc50953be1700f2ceafdf7b
Sashko <sashkoace@gmail.com>
2020-10-03 19:52:50 +0300

    added work section

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// ----------Hamburger----------
+
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     close = document.querySelector('.close');
@@ -10,4 +12,13 @@ hamburger.addEventListener('click', () => {
 
 close.addEventListener('click', () => {
     menu.classList.remove('menu_active');
+});
+
+// ----------SkillsAutoPercent----------
+
+const counters = document.querySelectorAll('.skills__percent-box-value'),
+    lines = document.querySelectorAll('.skills__percent-progress-value');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
 });
